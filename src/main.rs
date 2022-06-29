b7d1aaa0269c97a184e0e927248825e5e8f23309
Homenique Vieira <Homenique.t@gmail.com>
2022-06-29 18:33:57 -0300

    Criação do input na classe main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+   println!("Advinhe o número!");
+   println!("Digite o seu palpite.");
+
+   let mut palpite:String = String::new();
+
+   io::stdin().read_line(&mut palpite).expect("Falha ao ler a entrada");
+
+    println!("Você disse: {}", palpite);
 }
