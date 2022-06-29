31babaf215144a077721c784b37ab6181911ab67
Homenique Vieira <Homenique.t@gmail.com>
2022-06-29 19:55:38 -0300

    miniprojeto finalizado

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,30 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-    let mut palpite: String = String::new();
-    let _numero_secreto = rand::thread_rng().gen_range(1..101);
-
     println!("Advinhe o número!");
-    println!("Digite o seu palpite.");
-loop{
-    io::stdin()
-        .read_line(&mut palpite)
-        .expect("Falha ao ler a entrada");
+    let numero_secreto = rand::thread_rng().gen_range(1..101);
+    
+    loop {
+        let mut palpite: String = String::new();
+        println!("Digite o seu palpite.");
+        io::stdin()
+            .read_line(&mut palpite)
+            .expect("Falha ao ler a entrada");
 
-    let palpite: u32 = palpite.trim().parse().expect("Por favor digite um número!");
-
-    println!("Você disse: {}", palpite);
-    println!("O número secreto é: {}", _numero_secreto);
-    // Comparar o paçpite com o numero secreto
-    match palpite.cmp(&_numero_secreto) {
-        std::cmp::Ordering::Less => println!("Muito abaixo"),
-        std::cmp::Ordering::Greater => println!("Muito alto!"),
-        std::cmp::Ordering::Equal => println!("Você acertou!"),
+        let palpite: u32 = match palpite.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Você disse: {}", palpite);
+      
+        // Comparar o paçpite com o numero secreto
+        match palpite.cmp(&numero_secreto) {
+            std::cmp::Ordering::Less => println!("Muito abaixo"),
+            std::cmp::Ordering::Greater => println!("Muito alto!"),
+            std::cmp::Ordering::Equal => {
+                println!(">> > Você acertou! < <<");
+                break;
+            }
+        }
     }
-}
 }
