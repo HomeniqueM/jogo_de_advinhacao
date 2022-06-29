a7493c18c36ef1dddb9c000e3ed865958ca46bcb
Homenique Vieira <Homenique.t@gmail.com>
2022-06-29 19:05:43 -0300

    Agora é gerado o numero secreto

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
+
+
 fn main() {
+   let mut palpite:String = String::new();
+   let _numero_secreto = rand::thread_rng().gen_range(1..101);
+
    println!("Advinhe o número!");
    println!("Digite o seu palpite.");
 
-   let mut palpite:String = String::new();
 
    io::stdin().read_line(&mut palpite).expect("Falha ao ler a entrada");
 
     println!("Você disse: {}", palpite);
+    println!("O número secreto é: {}",_numero_secreto);
 }
